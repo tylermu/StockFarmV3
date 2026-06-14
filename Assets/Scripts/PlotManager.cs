414e6dd7eca0b2add4ecc6343902c24df2274156
tylermu <muchowtyler@gmail.com>
2026-06-14 12:22:24 -0500

    feat: Rudimentary plant and harvest crop

--- Assets/Scripts/PlotManager.cs
+++ Assets/Scripts/PlotManager.cs
@@ -2,20 +2,69 @@ using UnityEngine;
 
 public class PlotManager : MonoBehaviour
 {
+    bool isPlanted = false;
+    SpriteRenderer plant;
+    BoxCollider2D plantCollider;
+    public Sprite[] plantStages;
+    int plantStage = 0;
+    float timeBetweenStages = 2f;
+    float timer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        plant = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        plantCollider = transform.GetChild(0).GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         if(isPlanted)
+        {
+            timer -= Time.deltaTime;
+            if(timer < 0 && plantStage<plantStages.Length-1)
+            {
+                timer = timeBetweenStages;
+                plantStage++;
+                UpdatePlant();
+            }
+        }
     }
 
     private void OnMouseDown()
     {
-        Debug.Log("Clicked");
+        if(isPlanted)
+        {
+            if(plantStage == plantStages.Length-1)
+            {
+                Harvest();
+            }
+        } else
+        {
+            Plant();
+        }
+    }
+
+    void Harvest()
+    {
+        isPlanted = false;
+        plant.gameObject.SetActive(false);
+    }
+
+    void Plant()
+    {
+        isPlanted = true;
+        plantStage = 0;
+        UpdatePlant();
+        timer = timeBetweenStages;
+        plant.gameObject.SetActive(true);
+    }
+
+    void UpdatePlant()
+    {
+        plant.sprite = plantStages[plantStage];
+        plantCollider.size = plant.sprite.bounds.size;
+        plantCollider.offset = new Vector2(0,plant.bounds.size.y/2);
     }
 }
